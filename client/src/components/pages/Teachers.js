ad52de1699a0abcf8d7748061ff004e9a0dccfa6
Emre Kunduraci <kunduraci2@gmail.com>
2023-02-20 10:02:50 +0100

    Table Inputs Order

--- client/src/components/pages/Teachers.js
+++ client/src/components/pages/Teachers.js
@@ -5,6 +5,9 @@ import { Context } from "../../components/Context";
 // HOOKS
 import usePost from "../../hooks/usePost";
 
+// NPMS
+import $ from "jquery";
+
 const Teachers = () => {
   const { state } = useContext(Context);
 
@@ -20,9 +23,25 @@ const Teachers = () => {
   }, []);
 
   const blurHandle = (e) => {
+    const target = e.target;
     const value = e.target.value;
     const name = e.target.name;
     setBody({ name: value });
+    $(target).siblings("div").css("display", "grid");
+    $(target).css("display", "none");
+    setValue("");
+  };
+
+  const changeHandle = (e) => {
+    setValue((old) => old + e.target.value);
+  };
+
+  const clickHandle = (e) => {
+    const target = e.target;
+    const html = $(target).html();
+    $(target).siblings("input").css("display", "initial").focus();
+    $(target).css("display", "none");
+    setValue(html)
   };
 
   usePost(url, body, "get");
@@ -39,58 +58,79 @@ const Teachers = () => {
       <table>
         <tbody>
           <tr>
-            <th></th>
+            <th>NEW</th>
             <th>NAME</th>
             <th>SURNAME</th>
             <th>EMAIL</th>
             <th>PASSWORD</th>
             <th>DATE</th>
-            <th></th>
+            <th>DELETE</th>
           </tr>
           {state.data &&
             state.data.map((result) => {
               const { _id, name, surname, email, createdAt } = result;
               console.log(result);
-
               return (
-                <tr className="row" key={result._id}>
+                <tr className="row" key={_id}>
                   <td className="new">&#x2716;</td>
                   <td className="name">
+                    <div className="nameDiv" onClick={(e) => clickHandle(e)}>
+                      {name}
+                    </div>
                     <input
                       className="tdInput"
                       type="text"
-                      value={result.name}
+                      value={value}
                       name="name"
                       onBlur={(e) => blurHandle(e)}
+                      onChange={(e) => changeHandle(e)}
                     />
                   </td>
+
                   <td className="surname">
+                    <div className="nameDiv" onClick={(e) => clickHandle(e)}>
+                      {surname}
+                    </div>
                     <input
                       className="tdInput"
                       type="text"
-                      value={result.surname}
+                      value={value}
                       name="surname"
                       onBlur={(e) => blurHandle(e)}
+                      onChange={(e) => changeHandle(e)}
                     />
                   </td>
+
                   <td className="email">
+                    <div className="nameDiv" onClick={(e) => clickHandle(e)}>
+                      {email}
+                    </div>
                     <input
                       className="tdInput"
                       type="email"
-                      value={result.email}
+                      value={value}
                       name="email"
                       onBlur={(e) => blurHandle(e)}
+                      onChange={(e) => changeHandle(e)}
                     />
                   </td>
-                  <td className="date">
-                    <input className="tdInput" type="password" value="*****" />
-                  </td>
-                  <td className="date">
+                  <td className="password">
+                    <div className="nameDiv" onClick={(e) => clickHandle(e)}>
+                      ***********
+                    </div>
                     <input
                       className="tdInput"
-                      type="text"
-                      value={result.createdAt}
+                      type="email"
+                      value={value}
+                      name="email"
+                      onBlur={(e) => blurHandle(e)}
+                      onChange={(e) => changeHandle(e)}
                     />
+                  </td>
+                  <td className="date">
+                    <div className="nameDiv" onClick={(e) => clickHandle(e)}>
+                      01.01.2023
+                    </div>
                   </td>
                   <td className="delete">&#x2716;</td>
                 </tr>
