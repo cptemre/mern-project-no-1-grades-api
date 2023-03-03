097d95373fd03dfb9a871f8c6dd527af35feefc3
Emre Kunduraci <kunduraci2@gmail.com>
2023-03-03 10:52:46 +0100

    Pagination Function Is Finished
    Table should be separate component.\nLoading component should be showed on page change

--- client/src/components/pagination/Pagination.js
+++ client/src/components/pagination/Pagination.js
@@ -9,6 +9,7 @@ import useComponent from "../../hooks/useComponent";
 
 // NPMS
 import { useSearchParams } from "react-router-dom";
+import $ from "jquery";
 
 // FONT AWESOME
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -62,6 +63,34 @@ const Pagination = () => {
     setLastPage(10);
   }, [state[`${component}Length`]]);
 
+  // ARROWS FUNCTIONS TO SET SEARCH PARAMS
+  const leftArrowClickHandle = () => {
+    if (selectedPage != 1) {
+      setSearchParams((searchParams) => {
+        searchParams.set("pageValue", selectedPage - 1);
+        return searchParams;
+      });
+    }
+  };
+  const rightArrowClickHandle = () => {
+    if (selectedPage != lastPage) {
+      setSearchParams((searchParams) => {
+        searchParams.set("pageValue", selectedPage + 1);
+        return searchParams;
+      });
+    }
+  };
+
+  // NUMBERS FUNCTION TO SET SEARCH PARAMS
+  const numbersHandle = (e) => {
+    const value = $(e.currentTarget).html();
+    console.log(value);
+    setSearchParams((searchParams) => {
+      searchParams.set("pageValue", value);
+      return searchParams;
+    });
+  };
+
   // AXIOS CALL
   useFetch(
     fetchVars.url,
@@ -71,54 +100,78 @@ const Pagination = () => {
   );
   return (
     <section id="paginationSection">
-      <article id="leftContainer">
-        <FontAwesomeIcon icon="fa-solid fa-square-caret-left" />
+      <article
+        id="leftContainer"
+        className="buttonIcons"
+        onClick={() => leftArrowClickHandle()}
+        style={{
+          opacity: selectedPage == 1 && "0.5",
+          color: selectedPage == 1 && "white",
+        }}
+      >
+        <FontAwesomeIcon icon="fa-solid fa-square-caret-left" id="leftIcon" />
       </article>
       <article id="numbers">
-        {selectedPage !== 1 && <div className="numbers">1</div>}
-        <div id="earlierPageContainer">
-          {selectedPage - 2 > 2 ? (
-            <>
-              <div className="dots">...</div>
-              <div className="numbers">{selectedPage - 2}</div>
-              <div className="numbers">{selectedPage - 1}</div>
-            </>
-          ) : selectedPage - 2 > 1 ? (
-            <>
-              <div className="numbers">{selectedPage - 2}</div>
-              <div className="numbers">{selectedPage - 1}</div>
-            </>
-          ) : (
-            selectedPage - 2 > 0 && (
-              <>
-                <div className="numbers">{selectedPage - 1}</div>
-              </>
-            )
-          )}
-        </div>
-        {selectedPage && <div className="numbers">{selectedPage}</div>}
-        <div id="furtherPageContainer">
-          {selectedPage + 2 <= lastPage ? (
-            <>
-              <div className="numbers">{selectedPage + 1}</div>
-              <div className="numbers">{selectedPage + 2}</div>
-            </>
-          ) : (
-            selectedPage + 1 <= lastPage && (
-              <>
-                <div className="numbers">{selectedPage + 1}</div>
-              </>
-            )
-          )}
-        </div>
-
+        {selectedPage !== 1 && (
+          <div className="numbers" onClick={(e) => numbersHandle(e)}>
+            1
+          </div>
+        )}
+        {selectedPage - 2 > 2 && <div className="dots">...</div>}
+        {selectedPage - 2 > 1 && (
+          <div className="numbers" onClick={(e) => numbersHandle(e)}>
+            {selectedPage - 2}
+          </div>
+        )}
+        {selectedPage - 2 > 0 && (
+          <div className="numbers" onClick={(e) => numbersHandle(e)}>
+            {selectedPage - 1}
+          </div>
+        )}
+        {selectedPage && (
+          <div
+            className="numbers"
+            id="selectedPage"
+            onClick={(e) => numbersHandle(e)}
+          >
+            {selectedPage}
+          </div>
+        )}
+        {selectedPage + 2 <= lastPage && (
+          <>
+            <div className="numbers" onClick={(e) => numbersHandle(e)}>
+              {selectedPage + 1}
+            </div>
+          </>
+        )}
+        {selectedPage + 1 <= lastPage && (
+          <>
+            <div className="numbers" onClick={(e) => numbersHandle(e)}>
+              {selectedPage + 2}
+            </div>
+          </>
+        )}
         {lastPage > selectedPage + 3 && <div className="dots">...</div>}
         {lastPage > selectedPage + 2 && (
-          <div className="numbers">{lastPage}</div>
+          <div
+            className="numbers"
+            id="lastPage"
+            onClick={(e) => numbersHandle(e)}
+          >
+            {lastPage}
+          </div>
         )}
       </article>
-      <article id="rightContainer">
-        <FontAwesomeIcon icon="fa-solid fa-square-caret-right" />
+      <article
+        id="rightContainer"
+        className="buttonIcons"
+        onClick={() => rightArrowClickHandle()}
+        style={{
+          opacity: selectedPage == lastPage && "0.5",
+          color: selectedPage == lastPage && "white",
+        }}
+      >
+        <FontAwesomeIcon icon="fa-solid fa-square-caret-right" id="rigthIcon" />
       </article>
     </section>
   );
